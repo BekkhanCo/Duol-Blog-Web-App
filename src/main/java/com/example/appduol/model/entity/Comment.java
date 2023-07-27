432cb9b74c3c73427cec39dd611cca48b259d79e
Bekzodjon Valiyev <bekzodvaliev1@gmail.com>
2023-07-27 13:13:42 +0500

    Perf(model): Comment builder implimented.

--- src/main/java/com/example/appduol/model/entity/Comment.java
+++ src/main/java/com/example/appduol/model/entity/Comment.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "comments")
