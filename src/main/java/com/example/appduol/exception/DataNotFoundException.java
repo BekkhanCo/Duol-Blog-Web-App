e43b0d9e968b8a096d1f3debfa560dc78040bda4
Bekzodjon Valiyev <bekzodvaliev1@gmail.com>
2023-07-27 11:26:55 +0500

    Feature(exception): DataNotFoundException added.

---
+++ src/main/java/com/example/appduol/exception/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.appduol.exception;
+
+public class DataNotFoundException extends RuntimeException{
+
+  public DataNotFoundException(String message) {
+    super(message);
+  }
+}
