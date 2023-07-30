eecaa4b6aee94b2b20a2530367d0a7ea17408ea3
Bekzodjon Valiyev <bekzodvaliev1@gmail.com>
2023-07-30 23:17:27 +0500

    Refactor: Giving comment creation date replaced to anotation.

--- src/main/java/com/example/appduol/model/entity/Comment.java
+++ src/main/java/com/example/appduol/model/entity/Comment.java
@@ -1,17 +1,20 @@
 package com.example.appduol.model.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -26,9 +29,11 @@ public class Comment {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  @ManyToOne(cascade = CascadeType.ALL)
+  @ManyToOne(fetch =  FetchType.LAZY)
   private Blog blog;
   private String text;
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
   @Column(name = "created_date")
   private LocalDateTime createdDate;
   @Column(name = "count_of_useful")
