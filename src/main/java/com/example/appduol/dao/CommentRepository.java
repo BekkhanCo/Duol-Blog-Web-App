6adfc75c82ed3e1c94d6374bc44d82c8e0ac5858
Bekzodjon Valiyev <bekzodvaliev1@gmail.com>
2023-07-30 23:23:15 +0500

    Feature: Get comments by filter params and new delete functions implimented.
    Comments are filtered by checked field and blog id.
    
    Comments related to blog are deleted by blog id.

--- src/main/java/com/example/appduol/dao/CommentRepository.java
+++ src/main/java/com/example/appduol/dao/CommentRepository.java
@@ -1,11 +1,12 @@
 package com.example.appduol.dao;
 
+import com.example.appduol.model.entity.Blog;
 import com.example.appduol.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-
-  List<Comment> findAllByChecked(boolean checked);
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long>, JpaSpecificationExecutor<Comment> {
+  void deleteAllByBlog(Blog blog);
 }
--- src/main/java/com/example/appduol/service/CommentService.java
+++ src/main/java/com/example/appduol/service/CommentService.java
@@ -1,8 +1,8 @@
 package com.example.appduol.service;
 
-import com.example.appduol.model.entity.Blog;
 import com.example.appduol.model.entity.Comment;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -10,15 +10,20 @@ import java.util.Optional;
 @Service
 public interface CommentService {
 
+  @Transactional
   Optional<Comment> getById(Long id);
 
+  @Transactional
   Collection<Comment> getAll();
 
-  Collection<Comment> getAllByFilter(Boolean checked);
+  @Transactional
+  Collection<Comment> getAllByFilter(Boolean checked,Long blogId);
 
   Comment save(Comment comment);
 
   void delete(Comment comment);
+
+  void deleteAllByBlogId(Long id);
 
   Comment update(Long id, Comment comment);
 }
